dd6b94dc9e1b78e1d807233e47780049971db918
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-21 10:58:04 +0500

    react project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" }; // bu yozgan kodni babel ozgartirib beradi constructor function qoshadi
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => (this.state.lat = position.coords.latitude),
+      (err) => (this.state.errorMessage = err.message)
+    );
+  }
+  render() {
+    console.log(this.state.lat);
+    if (this.state.lat !== null) {
+      return (
+        <div>
+          <div>Latitude: {this.state.lat}</div>
+        </div>
+      );
+    }
+    if (this.state.errorMessage !== "") {
+      return (
+        <div>
+          <div>Error:{this.state.errorMessage}</div>
+        </div>
+      );
+    }
+    return <div>Loading....</div>;
+  }
+}
+ReactDOM.render(<App />, document.querySelector("#root"));
