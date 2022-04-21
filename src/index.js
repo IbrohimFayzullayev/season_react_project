5563869c8833f4aa0db7f4d2e9747b8e4f3630f0
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-04-21 19:46:58 +0500

    homework finished

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import DisplaySeason from "./DisplaySeason.js";
 
 class App extends React.Component {
-  state = { lat: null, errorMessage: "" }; // bu yozgan kodni babel ozgartirib beradi constructor function qoshadi
+  // state = { lat: null, errorMessage: "" }; // bu yozgan kodni babel ozgartirib beradi constructor function qoshadi
+
+  state = { lat: null, errorMessage: "" };
+
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-      (position) => (this.state.lat = position.coords.latitude),
-      (err) => (this.state.errorMessage = err.message)
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
-  render() {
-    console.log(this.state.lat);
-    if (this.state.lat !== null) {
+  getLocation() {
+    if (this.state.lat) {
       return (
         <div>
           <div>Latitude: {this.state.lat}</div>
         </div>
       );
     }
-    if (this.state.errorMessage !== "") {
+    if (this.state.errorMessage) {
       return (
         <div>
           <div>Error:{this.state.errorMessage}</div>
@@ -26,6 +29,11 @@ class App extends React.Component {
       );
     }
     return <div>Loading....</div>;
+  }
+  render() {
+    return (
+      <DisplaySeason lat={this.state.lat} error={this.state.errorMessage} />
+    );
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
